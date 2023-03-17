857f982d7b5453f66af83d4dea11c26b74282d33
przemjan <przemjanisz2@gmail.com>
2023-03-17 08:12:53 +0100

    Initial commit

---
+++ src/StringDemo.java
@@ -0,0 +1,20 @@
+public class StringDemo {
+    public static void main(String[] args) {
+
+        String name = "Genowefcia";
+
+        int dlugosc;
+
+        dlugosc = name.length();
+
+        char inicjal = name.charAt(3);
+
+        String duze, male;
+
+        duze = name.toUpperCase();
+        male = name.toLowerCase();
+
+        System.out.println(name);
+
+    }
+}
---
+++ src/Zmienne.java
@@ -0,0 +1,30 @@
+public class Zmienne {
+    public static void main(String[] args) {
+
+        int liczba;
+        liczba = 13;
+
+        double liczba2 = 14.9;
+
+        System.out.println("Moja liczba" + liczba);
+
+        int days = 5;
+        int hours = 8;
+        int weeks = 52;
+
+        int workHoursPerYear = days * hours * weeks;
+
+        System.out.println(workHoursPerYear);
+
+        String napis = "Dzisiaj\\ jest \"ładnie\"";
+
+        System.out.println("Nasz napis:" + napis + ".");
+        System.out.print("Nasz napis:" + napis + ".\n");
+        System.out.print("Nasz napis:" + napis + ".\n");
+
+        char znak = '#';
+
+        boolean sun = true; //false
+
+    }
+}
